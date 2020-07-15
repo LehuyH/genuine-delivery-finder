b0b8601626ff53df61908bcb29ac52fdacc69e90
LehuyH <lehuy.hoang@outlook.com>
2020-07-15 11:01:01 -0700

    Update basic ui

--- background/background.js
+++ background/background.js
@@ -4,7 +4,7 @@
 chrome.runtime.onMessage.addListener( (req, sender, sendResponse)=>{
 console.log(req.type)
     switch(req.type){
-        case "instacart":
+        case "Instacart":
             fetch('https://instacart.com/v3/retailers/1/delivery_options?source=web').then(response => response.json())
                 .then((data) =>{
                 console.log(data)
--- view/js/app.js
+++ view/js/app.js
@@ -1,19 +1,53 @@
-// This is where the work happens lol:
+// Provider Store ----
+let selectedProvider = null
+// Events-------
 
+//views
+document.querySelector("#start").addEventListener("click", () => router.go("Onboarding"))
+document.querySelectorAll(".return").forEach((e)=>{
+    e.addEventListener("click", () => router.go("Search"))
+   
+   })
+
+
+//select provider
 document.querySelector("#searchBtn")
-    .addEventListener("click", () => router.go("Loading"));
+    .addEventListener("click", async () => {
+        router.go("Loading")
+        let provider = document.querySelector("#provider").value
+        selectedProvider = provider
+        //Fetch data
+        chrome.runtime.sendMessage({
+            type: provider
+        }, function (res) {
+            //Check for errors
+            if (res.data.error) {
+                switch (res.data.error.message) {
+                    case "Unauthorized":
+                        router.go("loginError")
+                }
+
+            } else if (typeof res.data.service_options !== 'undefined') { // Success!
+                
+                if(res.data.service_options)
+                router.go('Success')
+            } else { //Random error
+                router.go('generalError')
+            }
+
+        });
+
+
+
+    });
 
 router.onShow.Loading = async () => {
     /*
         Fetch data, load cookies, etc. here
     */
-   // const cookies = await getCookies('https://www.instacart.com/store/checkout_v3',"_instacart_session")
-    var cookies = "error"
-    chrome.runtime.sendMessage({type: "instacart"}, function(res) {
-        document.getElementById('status').innerText = JSON.stringify(res.data)
-        router.go("Results")
-      });
-    
+    // const cookies = await getCookies('https://www.instacart.com/store/checkout_v3',"_instacart_session")
+
+
     // Timeout only for testing, remove later:
-  
+
 }
